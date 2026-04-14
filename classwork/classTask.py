b9000d6737bec6b085b25dc5b3c55ad186d9783e
Isaac Urban <kpomassiurban@gmail.com>
2026-04-14 23:39:26 +0100

    renamed my folders

---
+++ classwork/classTask.py
@@ -0,0 +1,21 @@
+a = 5
+b = 10
+
+a,b = b,a
+
+print(f"{a} is a")
+print(f"{b} is b\n \n")
+
+######################################
+
+num1 = 5
+num2 = 10
+
+temp = num1
+num1 = num2
+num2 = temp
+
+print(f"{num1} is num1")
+print(f"{num2} is num2")
+
+
