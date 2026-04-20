4f044cd4c2e3e5cf611fee91f4c47f010a3ee3ca
Isaac Urban <kpomassiurban@gmail.com>
2026-04-20 10:31:36 +0100

    Done with the multiplication task

---
+++ classwork/multiplicationTable.py
@@ -0,0 +1,9 @@
+"""
+START
+1. Create a loop that runs 10 times.
+2. then print the digit that multiply the count 
+STOP
+"""
+
+for number in range(1, 11):
+    print(f"6 x {number} = {6 * number}")
