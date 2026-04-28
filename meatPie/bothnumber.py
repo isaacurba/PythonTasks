c3e8451ac353ba8cd477c3140b0ecbd0423cac57
Isaac Urban <kpomassiurban@gmail.com>
2026-04-28 03:06:51 +0100

    Done with few meatpie tasks

---
+++ meatPie/bothnumber.py
@@ -0,0 +1,5 @@
+num_one = int(input("Enter first number"))
+num_two = int(input("Enter second number"))
+
+print(num_one)
+print(num_two)
---
+++ meatPie/plus.py
@@ -0,0 +1,2 @@
+number = int(input("Enter number to be added to 10 "))
+print(number + 10)
