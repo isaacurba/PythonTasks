0b2557fd96c7b764219edb139d334fe76651642c
Isaac Urban <kpomassiurban@gmail.com>
2026-05-10 03:47:37 +0100

    Deleted spicy snack cos i moved them into a new repo for Ebuka

--- spicySnacks/leap_year.py
+++
@@ -1,6 +0,0 @@
-year = int(input("Enter your leap year: "))
-
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print(f"{year} is a leap year")
-else:
-    print(f"{year} is not a leap year")
